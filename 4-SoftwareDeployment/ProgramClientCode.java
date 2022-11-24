26028a1fc2104deb7e8b61af8ca583f5988e1b0b
Mariia Sirenko <79446249+mariiasirenko2@users.noreply.github.com>
2022-11-25 00:21:43 +0200

    Add files via upload

---
+++ 4-SoftwareDeployment/ProgramClientCode.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Start:");
+    try {
+      Class.forName("org.postgresql.Driver");
+      Connection connection = DriverManager.getConnection(
+          "jdbc:postgresql://localhost:1521/sirenkodb",
+          "postgres", "abc123");
+
+      Statement stmt = connection.createStatement();
+      ResultSet rs = stmt.executeQuery("select * from project");
+
+      while (rs.next()) {
+        System.out.println(rs.getString(1) + " | " 
+        + rs.getString(2) + " |"
+        + rs.getString(3) + " |"
+        + rs.getString(4) + " |"
+        + rs.getString(5) + " |");
+      }
+
+      connection.close();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    System.out.println("End");
+  }
+}
